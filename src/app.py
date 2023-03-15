91c29f862b657b519b04a9c2bfa89885a0701875
jdaneris <70027623+jdaneris@users.noreply.github.com>
2023-03-14 20:32:18 -0500

    first commit

---
+++ src/app.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template,url_for,redirect,request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    print('index')
+    return render_template('index.html')
+
+
+@app.route('/K_Means')
+def k_means():
+    return render_template('K_Means.html')
+
+@app.route('/Power_BI')
+def power_BI():
+    return render_template('Power_BI.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
